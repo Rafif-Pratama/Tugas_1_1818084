4830c932abaa9b9e95b14cbf64b4e05090e64205
Rafif Pratama <rafifpt13@gmail.com>
2020-10-19 08:53:06 +0700

    First commit

---
+++ app/src/main/java/com/example/profil_ui_design/profil_design.java
@@ -0,0 +1,14 @@
+package com.example.profil_ui_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profil_design extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_design);
+    }
+}
